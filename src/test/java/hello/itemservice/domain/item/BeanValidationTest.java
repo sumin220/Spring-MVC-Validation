0117b5b301964203cf0604b643ab2da11341bd48
sonsumin <sonsumin00@gmail.com>
2024-09-17 15:19:02 +0900

    Feat: Bean Validation 시작

---
+++ src/test/java/hello/itemservice/domain/item/BeanValidationTest.java
@@ -0,0 +1,38 @@
+package hello.itemservice.domain.item;
+
+import org.junit.jupiter.api.Test;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BeanValidationTest {
+
+
+    @Test
+    public void beanValidation() throws Exception {
+        //given
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+        //when
+
+        Item item = new Item();
+        item.setItemName(" ");
+        item.setPrice(0);
+        item.setQuantity(10000);
+
+        Set<ConstraintViolation<Item>> violations = validator.validate(item);
+        for (ConstraintViolation<Item> violation : violations) {
+            System.out.println("violation = " + violation);
+            System.out.println("violation.getMessage() = " + violation.getMessage());
+        }
+        //then
+
+    }
+
+}
